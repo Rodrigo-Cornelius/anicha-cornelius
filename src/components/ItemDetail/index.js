3a0ad965215fc77bf9583c26f543dfe9033b3588
Rodrigo Cornelius <ro6_1@hotmail.com>
2021-04-01 19:31:59 -0300

    Entrega de la clase 9

--- src/components/ItemDetail/index.js
+++ src/components/ItemDetail/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import styles from './ItemDetail.module.css';
 import ItemCount from '../ItemCount';
+import {Link} from 'react-router-dom';
 
 export default function ItemDetail({item={}}) {
     const [elementosCarrito, setElementosCarrito] = useState(0);
@@ -16,8 +17,16 @@ export default function ItemDetail({item={}}) {
                     <p>{item.description}</p> 
                     <p className='text-end fs-5'>$ {item.price}</p>
                     <p>Elementos enviados al carrito: {elementosCarrito}</p>
-                    <ItemCount stock={5} inicial={0} onAdd={setElementosCarrito}/>
-
+                    {
+                        elementosCarrito === 0 ?
+                            <ItemCount stock={5} inicial={0} onAdd={setElementosCarrito}/>
+                        :
+                        <Link to='/cart' className={`d-flex align-items-center justify-content-center mt-4 ${styles.btn_link}`}>
+                            <button type="button" className={`btn btn-primary ${styles.btn_link_terminarCompra}`}>
+                                Terminar mi compra
+                            </button>
+                        </Link>
+                    }
                 </div>
             </div>
         </div>
